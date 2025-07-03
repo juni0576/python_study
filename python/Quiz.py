c5762979971959893f52a7686025b79ced0d6261
junhee seo <helios.junhee@gmail.com>
2025-07-03 15:52:28 +0900

    1번 연습문제

---
+++ python/Quiz.py
@@ -0,0 +1,13 @@
+# Quiz 01
+x = int(input("첫번째 숫자를 입력하세요"))
+y = int(input("두번째 숫자를 입력하세요"))
+
+print("두 수의 합: ", x+y)
+print("두 수의 차: ", x-y)
+print("두 수의 곱: ", x*y)
+print("두 수의 평균: ", (x+y)/2)
+print("두 수 중 큰 수: ", max(x, y))
+print("두 수 중 작은 수: ", min(x, y))
+
+# Quiz 02
+
