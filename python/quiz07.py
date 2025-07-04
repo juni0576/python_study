8780a0179d122ff00f1533d29a322f4f906f47c4
junhee seo <helios.junhee@gmail.com>
2025-07-04 09:29:12 +0900

    7번 문제 abs 지움. 큰 의미는 없는 것 같아서.

--- python/quiz07.py
+++ python/quiz07.py
@@ -4,5 +4,5 @@ y1=int(input("첫번째 y좌표"))
 x2=int(input("두번째 x좌표"))
 y2=int(input("두번째 y좌표"))
 
-distance = math.sqrt(abs(x2 - x1)**2 + abs(y2 - y1)**2)
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 print(distance)
